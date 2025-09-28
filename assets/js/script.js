48c2fe3ee28920f785d51fa49a8b37e5ae12f22f
ilqarsfv <ilqar.sofiyev.95@gmail.com>
2025-09-28 14:58:16 +0400

    v1.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,35 @@ $(document).ready(() => {
         e.stopPropagation()
         $(this).toggleClass("active")
     })
+    $("#main-button").click(function () {
+        $(this).toggleClass("open")
+        $(".closeChat").toggleClass("hidden");
+        $(".openChat").toggleClass("hidden");
 
+    })
+    $("#modalContentTop svg, .getModal").click(() => {
+        $("#myModal").toggleClass("hidden")
+    })
+    $('#myModal').on('click', function (e) {
+        if (!$(e.target).closest('.modalContent').length) {
+            $(this).addClass('hidden');
+        }
+    });
     $(document).click(function (e) {
         if (!$(e.target).closest('.dropdown, .dropdownMenu').length) {
             $(".dropdown").removeClass("active");
         }
+    });
+
+    const banner = new Swiper(".heroSwiper", {
+        autoplay: {
+            delay: 3500,
+            disableOnInteraction: false,
+        },
+        loop: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
     });
 })
